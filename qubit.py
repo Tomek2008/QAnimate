b86ac1f229f0927272e6c389101e499f686a8f44
Tomek2008 <tomek.zmuda@outlook.com>
2026-07-18 13:17:00 +0200

    add Qubit class with ValueTracker

--- qubit.py
+++ qubit.py
@@ -1,5 +1,7 @@
 import numpy as np
 from manim import *
+from constants import *
+from bloch_sphere import BlochSphere2D
 
 class Qubit:
     def __init__(self, theta, phi):
@@ -14,4 +16,10 @@ class Qubit:
     def one(cls):
         return cls(1, 1)
 
-    def get_vector(selfs):
+    def get_vector(self, sphere):
+        end = sphere.point_at(self.theta.get_value(), self.phi.get_value())
+        start = sphere.outline.get_center()
+        return Arrow(start, end, color=VECTOR_COLOR, stroke_width=2, max_tip_length_to_length_ratio=0.1)
+
+    def set_angles(self, theta, phi):
+        return [self.theta.animate.set_value(theta), self.phi.animate.set_value(phi)]
