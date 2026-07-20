921b83325eb604b94ac2bd5a9100d06d278eaf31
Tomek2008 <tomek.zmuda@outlook.com>
2026-07-20 19:09:07 +0200

    changed test scene to work with 3d and created readme

--- scenes/test_bloch_conversion.py
+++ scenes/test_bloch_conversion.py
@@ -1,28 +1,26 @@
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from bloch_sphere import BlochSphere2D
+from bloch_sphere import BlochSphere3D
 from qubit import Qubit
 from manim import *
 
-class BlochSphereCheck(Scene):
+class BlochSphereCheck(ThreeDScene):
     def construct(self):
-        sphere = BlochSphere2D(radius=3)
-        self.play(Create(sphere))
+        self.set_camera_orientation(phi=65 * DEGREES, theta=-45 * DEGREES)
 
+        sphere = BlochSphere3D(radius=1.5, show_labels=True)
+        bloch_sphere = VGroup(sphere.surface, sphere.equator, *sphere.meridians, sphere.axes)
         q = Qubit.zero()
         vector = sphere.vector(q)
-        self.play(Create(vector))
-        self.wait(0.5)
 
-        self.play(*q.set_angles(np.pi / 2), run_time=1.5)
-        self.wait(0.5)
+        self.add(vector)
+        self.add(bloch_sphere)
+        self.add_fixed_orientation_mobjects(*sphere.labels)
 
-        self.play(*q.set_angles(np.pi, 0), run_time=1.5)
-        self.wait(0.5)
-
-        self.play(*q.set_angles(np.pi / 2, np.pi), run_time=1.5)
-        self.wait()
-
-        self.play(*q.set_angles(np.pi / 1.3, np.pi), run_time=1.5)
-        self.wait()
+        self.begin_ambient_camera_rotation(rate=2 * PI / 10)
+        self.play(*q.set_angles(np.pi / 2, 0), run_time=2.5)
+        self.play(*q.set_angles(np.pi, 0), run_time=2.5)
+        self.play(*q.set_angles(0, np.pi), run_time=2.5)
+        self.play(*q.set_angles(0, np.pi / 2), run_time=2.5)
+        self.stop_ambient_camera_rotation()
