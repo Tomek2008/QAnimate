b98243b6e32c98ffaa8d87bee338ce991fe1cfd5
Tomek2008 <tomek.zmuda@outlook.com>
2026-08-31 14:09:04 +0200

    update qubit for gate transformations

--- qanimate/qubit.py
+++ qanimate/qubit.py
@@ -1,5 +1,20 @@
 from .constants import *
 
+
+def bloch_to_statevector(theta, phi):
+    return np.array([
+        np.cos(theta / 2),
+        np.exp(1j * phi) * np.sin(theta / 2),
+    ])
+
+
+def statevector_to_bloch(state):
+    a, b = state
+    theta = 2 * np.arccos(np.clip(np.abs(a), -1, 1))
+    phi = np.angle(b) - np.angle(a)
+    return theta, phi % (2 * np.pi)
+
+
 class Qubit:
     def __init__(self, theta, phi, r=1):
         self.theta = ValueTracker(theta)
@@ -16,3 +31,12 @@ class Qubit:
 
     def set_angles(self, theta, phi):
         return [self.theta.animate.set_value(theta), self.phi.animate.set_value(phi)]
+
+    def gate_angles(self, gate_matrix):
+        state = bloch_to_statevector(self.theta.get_value(), self.phi.get_value())
+        new_state = gate_matrix @ state
+        return statevector_to_bloch(new_state)
+
+    def apply_gate(self, gate_matrix):
+        theta, phi = self.gate_angles(gate_matrix)
+        return self.set_angles(theta, phi)
